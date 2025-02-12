8897ca3a90ff7558f20b1c5168c05b1131a6bc94
Nathan W <weidenhamer.nathan@gmail.com>
2025-02-12 08:57:46 -0700

    Database transfer... need to fix the large table size.

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -3,8 +3,42 @@ import './database.css';
 
 export function Scores() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>scores displayed here</div>
-    </main>
+    <main className="container-fluid text-center">
+                <p><h2>Wins Database</h2></p>
+                <table className="table table-striped-columns">
+                    <tr>
+                        <th>Rank</th>
+                        <th>Username</th>
+                        <th>Wins</th>
+                        <th>Losses</th>
+                        <th>Draws</th>
+                        <th>Win Percentage</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Special User</td>
+                        <td>10</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>100%</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Less Special User</td>
+                        <td>5</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>50%</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Not Special User</td>
+                        <td>0</td>
+                        <td>10</td>
+                        <td>0</td>
+                        <td>0%</td>
+                    </tr>
+                </table>
+        </main>
   );
 }
