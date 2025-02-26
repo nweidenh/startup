204fd26e39edff61842e6978eba51bcf6266e1a4
Nathan W <weidenhamer.nathan@gmail.com>
2025-02-26 14:55:12 -0700

    Game Win scenarios

--- src/play/gameplay.jsx
+++ src/play/gameplay.jsx
@@ -8,6 +8,7 @@ export function Gameplay(props){
     const Username = props.Username;
     const [turnState, setTurnState] = React.useState(props.turnState)
     const [moveResult, setMoveResult] = React.useState('')
+    const [winner, setWinner] = React.useState('')
 
     const [data, setData] = useState([
         { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0 },
@@ -43,19 +44,77 @@ export function Gameplay(props){
                 return row;
             }).reverse();
             if (updated == true){
-                setTurnState(nextTurn)
-                setMoveResult("Yes!")
+                setTurnState(nextTurn);
+                setMoveResult("Yes!");
             } else{
-                setMoveResult("No, please try again")
+                setMoveResult("No, please try again");
             }
-            return newData;
+            if(checkWin(newData) === 1){
+                winner = setWinner("Congrats! You Win!");
+            } else if(checkWin(newData) === -1){
+                winner = setWinner("Sorry to say it, but the computer beat you!");
+            } else{
+                winner = setWinner("No winner yet...")
+            }
+                return newData;
         });
     }
+
+    const checkWin = (board) => {
+        const rows = board.length;
+        const cols = Object.keys(board[0]).length;
+        const directions = [
+            {r:0, c:1},
+            {r:1, c:0},
+            {r:1, c:1},
+            {r:1, c:-1}
+        ];
+
+        const colKeys = Object.keys(board[0]);
+
+        const getCell = (r, c) => (board[r] && board[r][colKeys[c]]) ?? null;
+
+        for(let r=0; r < rows; r++){
+            for(let c=0; c<cols; c++){
+                const cellValue = getCell(r,c);
+                if (cellValue === 0) continue;
+                for (let {r:dr, c:dc} of directions){
+                    let count = 1;
+                    let nr = r + dr;
+                    let nc = c + dc;
+
+                    while (
+                        nr >= 0 &&
+                        nr < rows &&
+                        nc >= 0 &&
+                        nc < cols &&
+                        getCell(nr, nc) ===cellValue
+                    ) {
+                        count++
+                        if(count ==4) return cellValue;
+                        nr += dr;
+                        nc += dc;
+                    }
+                }
+                // if(board[r][colKeys[c]] === 1){
+                //     if(board[r+1][colKeys[c]] === 1){
+                //         if (board[r+2] [colKeys[c]] === 1){
+                //             if (board[r+3] [colKeys[c]] === 1){
+                //                 winner = true;
+                //             }
+                //         }
+                //     }
+                // } else if (board[r]
+            }
+        }
+    }
+
+
 
     return (
         <main className="container-fluid">
                     {/*<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLive"> Forfeit Game</button>*/}
-                    <br />
+                    <h1>And the Winner is? {winner}</h1>
                     <table id="table-data" className="table table-bordered table-warning">
                       <thead>
                         <tr>
