10350c7905c77f403701cafde82e657c22b7fa37
Nathan W <weidenhamer.nathan@gmail.com>
2025-03-11 11:51:23 -0600

    Creation of login and create user endpoints Created service folder including npm downloads

---
+++ service/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const { createServerModuleRunner } = require('vite');
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+let users = [];
+let results = [];
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+app.use(express.json());
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter)
+
+//Create a new User
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await finduser('username', req.body.username)){
+        res.status(409).send({ msg: 'Existing User'});
+    } else {
+        const user = await createUser(req.body.username, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({email: user.email});
+    }
+});
+
+//Login as an Existing User
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('username', req.body.username);
+    if (user){
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4()
+            setAuthCookie(res, user.token);
+            res.send({username: user.username});
+            return;
+        }
+    }
+    res.status(401).send({msg: 'Unauthorized User'});
+});
+
