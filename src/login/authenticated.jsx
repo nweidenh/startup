ec3e86195e4d243c4d1448f721649e9647cbabb5
Nathan W <weidenhamer.nathan@gmail.com>
2025-03-12 11:24:45 -0600

    I chagned the buttons off of a form and it is finally working

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import Button from 'react-bootstrap/Button';
 
 export function Authenticated(props) {
     const navigate = useNavigate();
@@ -20,14 +21,12 @@ export function Authenticated(props) {
     }
   
     return (
-      <main className="container-fluid">
-      <form>
+      <div className="container-fluid">
       <div><h3 className="white-bold-text p-3">Welcome {props.Username}</h3></div>
       <div><h3 className="white-bold-text p-3">To play Connect4, please click the play button</h3></div>   
-        <button onClick={() => navigate('/play')} type="submit" className="btn btn-primary">Play</button>
-        <button onClick={() => logout()} type="submit" className="btn btn-secondary">Logout</button>
-      </form>
+        <Button onClick={() => navigate('/play')} type="submit" className="btn btn-primary">Play</Button>
+        <Button onClick={() => logout()} type="submit" className="btn btn-secondary">Logout</Button>
       <br />
-  </main>
+  </div>
     );
 }
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -28,33 +28,51 @@ export function Unauthenticated(props) {
         },
       });
       if (response?.status === 200) {
-        localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+        localStorage.setItem('Username', Username);
+        props.onLogin(Username);
       } else {
         const body = await response.json();
-        setDisplayError(`⚠ Error: ${body.msg}`);
+        setDisplayError(`Error: ${body.msg}`);
       }
     }
 
   
     return (
-      <main className="container-fluid">
-        <form>
-      <div><h3 className="white-bold-text p-3">To play a game, please login below:</h3></div>
-          <div className="input-group custom-padding">
-              <span className="input-group-text">Username:</span>
-              <input className="form-control" type="text" id="text" name="varText" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-          </div>
-          <div className="input-group custom-padding">
-              <span className="input-group-text">Password: </span>
-              <input className="form-control" type="password" id="password" name="varPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-          </div>
-              <button onClick={() => loginUser()} type="submit" className="btn btn-primary" disabled={!Username || !Password}>Login</button>
-              <button onClick={() => createUser()} type="submit" className="btn btn-secondary" disabled={!Username || !Password}>Create</button>
-      </form>
-      <br />
-      <p style={{ color: "red" }}> {displayError} </p>
-  </main>
+  //     <main className="container-fluid">
+  //       <form>
+  //     <div><h3 className="white-bold-text p-3">To play a game, please login below:</h3></div>
+  //         <div className="input-group custom-padding">
+  //             <span className="input-group-text">Username:</span>
+  //             <input className="form-control" type="text" id="text" name="varText" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
+  //         </div>
+  //         <div className="input-group custom-padding">
+  //             <span className="input-group-text">Password: </span>
+  //             <input className="form-control" type="password" id="password" name="varPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+  //         </div>
+  //             <button onClick={() => loginUser()} type="submit" className="btn btn-primary" disabled={!Username || !Password}>Login</button>
+  //             <button onClick={() => createUser()} type="submit" className="btn btn-secondary" disabled={!Username || !Password}>Create</button>
+  //     </form>
+  //     <br />
+  // </main>
+  <>
+  <div>
+    <div className='input-group mb-3'>
+      <span className='input-group-text'>Username:</span>
+      <input className='form-control' type='text' value={Username} onChange={(e) => setUsername(e.target.value)} />
+    </div>
+    <div className='input-group mb-3'>
+      <span className='input-group-text'>Password:</span>
+      <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} />
+    </div>
+    <Button variant='primary' onClick={() => loginUser()} disabled={!Username || !Password}>
+      Login
+    </Button>
+    <Button variant='secondary' onClick={() => createUser()} disabled={!Username || !Password}>
+      Create
+    </Button>
+  </div>
 
+  <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+</>
     );
 }
