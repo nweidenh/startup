d143b2215183bee2d50593620ada5bd3bfd58b70
Nathan W <weidenhamer.nathan@gmail.com>
2025-04-07 08:07:14 -0600

    Created on connection, message and pong

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -3,5 +3,19 @@ const { WebSocketServer } = requre('ws');
 function peerProxy(httpServer) {
     const socketServer = new WebSocketServer({server: httpServer})
 
-    
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach(function each(client) {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
+    });
 }
